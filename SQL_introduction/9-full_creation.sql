74788e02d010c31cb0a09e5da0ac868f833526b5
007codename <juri.malaj@gmail.com>
2026-09-11 17:50:07 +0200

    task 9: finished and ready for test correction

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
--- TASK 9
-CREATE TABLE IF NOT EXIST second_table(
+-- Create a table in database
+CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT
 );
 INSERT INTO second_table (id, name, score)
-VALUES (1, 'John', 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+VALUES(1,"John", 10),(2,"Alex", 3),(3,"Bob", 14),(4,"George", 8);
